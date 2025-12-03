23d24b06eb82811b5cf5bae1cdbe49f2234c7e54
Rúni Vørmadal <rev@ski.dk>
2025-12-03 20:52:46 +0100

    aoc2025

---
+++ AdventOfCode2025/AdventOfCode2025/Day1.cs
@@ -0,0 +1,54 @@
+﻿using System.Reflection;
+
+namespace AdventOfCode2025;
+
+public class Day1
+{
+    [Theory]
+    [InlineData("AdventOfCode2025.Input.d1ps.txt", 3, false)]
+    [InlineData("AdventOfCode2025.Input.d1p1.txt", 1023, false)]
+    [InlineData("AdventOfCode2025.Input.d1ps.txt", 6, true)]
+    [InlineData("AdventOfCode2025.Input.d1p1.txt", 5899, true)]
+    public void Puzzle1(string inputFile, int expected, bool countRoundTrips)
+    {
+        // get input from embedded resource
+        var assembly = Assembly.GetExecutingAssembly();
+        Stream input = assembly.GetManifestResourceStream(inputFile);
+        // use text reader
+        using StreamReader reader = new StreamReader(input);
+        string content = reader.ReadToEnd();
+
+        var lines = content.Split("\r\n", StringSplitOptions.RemoveEmptyEntries);
+
+        var zeroCounts = 0;
+        var dial = 50;
+        foreach (var line in lines)
+        {
+            var direction = line[0] == 'L' ? -1 : 1;
+
+            int number = int.Parse(line[1..]) * direction;
+
+            int prevDial = dial;
+            dial += number;
+            var roundTrips = Math.Abs(dial / 100);
+
+
+            if (countRoundTrips)
+            {
+                if (prevDial > 0 && dial < 0) zeroCounts++;
+                if (roundTrips == 0 && dial == 0) zeroCounts++;
+                zeroCounts += roundTrips;
+            }
+
+            dial %= 100;
+            if (dial < 0) dial = 100 + dial;
+
+            if (!countRoundTrips && dial == 0) zeroCounts++;
+        }
+
+
+        Assert.Equal(expected, zeroCounts);
+    }
+
+
+}
---
+++ AdventOfCode2025/AdventOfCode2025/Day2.cs
@@ -0,0 +1,54 @@
+﻿using System.Reflection;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode2025;
+
+public class Day2
+{
+    [Theory]
+    [InlineData("AdventOfCode2025.Input.d2ps.txt", 1227775554, false)]
+    [InlineData("AdventOfCode2025.Input.d2p1.txt", 28844599675, false)]
+    [InlineData("AdventOfCode2025.Input.d2ps.txt", 4174379265, true)]
+    [InlineData("AdventOfCode2025.Input.d2p1.txt", 48778605167, true)]
+    public void Puzzle1(string inputFile, long expected, bool multipleMatches)
+    {
+        // get input from embedded resource
+        var assembly = Assembly.GetExecutingAssembly();
+        Stream input = assembly.GetManifestResourceStream(inputFile);
+        // use text reader
+        using StreamReader reader = new StreamReader(input);
+        string content = reader.ReadToEnd();
+
+        var lines = content.Split(',', StringSplitOptions.RemoveEmptyEntries);
+        List<string> matches = [];
+        foreach (var line in lines)
+        {
+            var parts = line.Split('-');
+            var first = long.Parse(parts[0]);
+            var second = long.Parse(parts[1]);
+
+            for (long i = first; i <= second; i++)
+            {
+                var str = i.ToString();
+                var chars = str.ToCharArray();
+                var repititions = chars.GroupBy(c => c).OrderBy(c => c.Count()).First().Count();
+
+                for (var rep = repititions; rep > 1; rep--)
+                {
+                    if (str.Length % rep != 0 || (!multipleMatches && rep != 2)) continue;
+                    var val = str[..(str.Length / rep)];
+                    var test = new Regex(@$"^({val}){{{rep}}}$").Match(str);
+
+                    if (test?.Success == true)
+                    {
+                        matches.Add(str);
+                    }
+                }
+            }
+        }
+
+        Assert.Equal(expected, matches.Distinct().Select(long.Parse).Sum());
+    }
+
+
+}
